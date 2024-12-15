74c496808aff400030405ad314ef03dbb60c035e
Kaz Ueno <kazun.ue@gmail.com>
2024-12-15 23:13:03 +0900

    modify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@ module.exports = {
   extends: "@cybozu/eslint-config/presets/node-prettier",
   env: {
     mocha: true,
+    node: true,
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2015
   },
 };
