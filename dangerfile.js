bf64221d68f63d062d46c7b14593b168230bda3f
Kaz Ueno <kazun.ue@gmail.com>
2024-12-15 23:27:53 +0900

    modify dangerjs

--- dangerfile.js
+++ dangerfile.js
@@ -1,14 +1,21 @@
-import { danger, warn } from "danger";
+const { danger, warn } = require("danger");
+
+// isAllCheckPassedの初期化
+let isAllCheckPassed = true;
 
 // 500行以上の追加・削除の変更があったかどうか
-const diffSize = Math.max(danger.github.pr.additions, danger.github.pr.deletions);
+const diffSize = Math.max(
+  danger.github.pr.additions,
+  danger.github.pr.deletions
+);
+
 if (diffSize > 200) {
   warn("Should reduce diffs less than 500");
   isAllCheckPassed = false;
 }
 
-// 変更したファイルの数が10より多いかどうか
-if (danger.github.pr.changed_files > 1 ) {
+// 変更したファイルの数が1より多いかどうか
+if (danger.github.pr.changed_files > 1) {
   warn("Should reduce change files less than 10");
   isAllCheckPassed = false;
 }
