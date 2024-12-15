5ad3c450dafd730706657949634b88b27a36f9a5
Kaz Ueno <kazun.ue@gmail.com>
2024-12-15 23:36:33 +0900

    modify dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -6,7 +6,7 @@ let isAllCheckPassed = true;
 // 500行以上の追加・削除の変更があったかどうか
 const diffSize = Math.max(
   danger.github.pr.additions,
-  danger.github.pr.deletions
+  danger.github.pr.deletions,
 );
 
 if (diffSize > 200) {
@@ -18,4 +18,8 @@ if (diffSize > 200) {
 if (danger.github.pr.changed_files > 1) {
   warn("Should reduce change files less than 10");
   isAllCheckPassed = false;
+}
+
+if (isAllCheckPassed) {
+  markdown('## All checkes have passed');
 }
