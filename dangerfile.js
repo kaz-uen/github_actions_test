155077c3aa5780f2e21049408b07bf3401ac34a3
Kaz Ueno <kazun.ue@gmail.com>
2024-12-18 22:53:37 +0900

    modify

--- dangerfile.js
+++ dangerfile.js
@@ -1,16 +1,15 @@
 /* eslint-disable node/no-unpublished-require */
 const { danger, warn } = require("danger");
 
-// const totalLinesChanged = danger.git.linesAdded + danger.git.linesDeleted;
 const totalLinesChanged = danger.github.pr.additions + danger.github.pr.deletions;
 const filesChanged = danger.git.modified_files.length + danger.git.created_files.length + danger.git.deleted_files.length;
 
 // 200行以上の追加・削除の変更があったかどうか
-if (totalLinesChanged > 10) {
-  warn(`合計 ${totalLinesChanged} 行が変更されています！10行を超えています。`);
+if (totalLinesChanged > 200) {
+  warn(`合計 ${totalLinesChanged} 行が変更されています。200行以上の追加・削除は違反です!`);
 }
 
-// 変更したファイルの数が1より多いかどうか
-if (filesChanged > 1) {
-  warn(`合計 ${filesChanged} ファイルが変更されています！1ファイルを超えています。`);
+// 変更したファイルの数が10より多いかどうか
+if (filesChanged > 10) {
+  warn(`合計 ${filesChanged} ファイルが変更されています。10ファイル以上の変更は違反です!`);
 }
