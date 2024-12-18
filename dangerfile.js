e888170ada15b0f66071d62bb1739894c85d99f3
Kaz Ueno <kazun.ue@gmail.com>
2024-12-18 21:45:06 +0900

    modify

--- dangerfile.js
+++ dangerfile.js
@@ -5,8 +5,8 @@ const totalLinesChanged = danger.git.linesAdded + danger.git.linesDeleted;
 const filesChanged = danger.git.modified_files.length + danger.git.created_files.length + danger.git.deleted_files.length;
 
 // 200行以上の追加・削除の変更があったかどうか
-if (totalLinesChanged > 1) {
-  warn(`合計 ${totalLinesChanged} 行が変更されています！1行を超えています。`);
+if (totalLinesChanged > 10) {
+  warn(`合計 ${totalLinesChanged} 行が変更されています！10行を超えています。`);
 }
 
 // 変更したファイルの数が1より多いかどうか
